404a400cef969a9c4e445bda6f6badee7f7cf4a8
AbbasDehnad <83868773+AbbasDehnad@users.noreply.github.com>
2022-07-06 17:17:34 +0200

    change model according to specification and migrate

--- datacenters/centers/models.py
+++ datacenters/centers/models.py
@@ -2,19 +2,15 @@ from django.db import models
 
 # Create your models here.
 
+
 class CentersModel(models.Model):
-    name = models.CharField(max_length=100)
-    address = models.CharField(max_length=100)
-    city = models.CharField(max_length=100)
-    state = models.CharField(max_length=100)
-    zipcode = models.CharField(max_length=100)
-    phone = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-    url = models.CharField(max_length=100)
-    latitude = models.CharField(max_length=100)
-    longitude = models.CharField(max_length=100)
+    interface_name = models.CharField(max_length=100, blank=True, null=True)
+    port = models.IntegerField(blank=True, null=True)
+    vlan_id = models.IntegerField(blank=True, null=True)
+    ipv6_address = models.CharField(max_length=100,   blank=True, null=True)
+    prefix_length = models.IntegerField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.name
+        return f"{self.interface_name} {self.vlan_id} {self.created_at} "
