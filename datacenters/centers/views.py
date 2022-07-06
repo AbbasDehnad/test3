d019bd4f39945e9cefa3dd7f1d064c524861852d
AbbasDehnad <83868773+AbbasDehnad@users.noreply.github.com>
2022-07-06 16:54:56 +0200

    create listview for centers

--- datacenters/centers/views.py
+++ datacenters/centers/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from datacenters.centers.models import CentersModel
 
 # Create your views here.
+class CentersListView(ListView):
+    model = CentersModel
+    template_name = 'centers/centers_list.html'
+    context_object_name = 'centers'
+    paginate_by = 10
+    queryset = CentersModel.objects.all()
