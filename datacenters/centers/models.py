78600411cb2c9284848b835b956ce2f2e1b36855
AbbasDehnad <83868773+AbbasDehnad@users.noreply.github.com>
2022-07-06 16:51:13 +0200

    create centersmodel and migrate

--- datacenters/centers/models.py
+++ datacenters/centers/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class CentersModel(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    url = models.CharField(max_length=100)
+    latitude = models.CharField(max_length=100)
+    longitude = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
